b39c05b18d3c3ac8f0acb79c7c0240667b4539b0
AgustinCamassa <agustincamassa553@outlook.com>
2023-07-30 15:35:55 -0300

    Corrección carrusel

--- src/components/Paginas/Nosotros/Nosotros.jsx
+++ src/components/Paginas/Nosotros/Nosotros.jsx
@@ -32,36 +32,42 @@ const Nosotros = () => {
             <Carousel.Item>
               <img
                 className="d-block imCar w-100"
-                src="./libros.png"
+                src="slide1.png"
                 alt="First slide"
               />
               <Carousel.Caption>
-                <h5>First slide label</h5>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
                 className="d-block imCar w-100"
-                src="./libros.png"
+                src="./slide2.png"
                 alt="Second slide"
               />
               <Carousel.Caption>
-                <h5>Second slide label</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
                 className="d-block imCar w-100"
-                src="./libros.png"
+                src="slide3.png"
                 alt="Third slide"
               />
               <Carousel.Caption>
-                <h5>Third slide label</h5>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                </p>
+
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <img
+                className="d-block imCar w-100"
+                src="./slide4.png"
+                alt="Second slide"
+              />
+              <Carousel.Caption>
+
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
